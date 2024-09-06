38ad3478ca9703eb481f1b6e30f7447b98c6f22e
rianahasna <rianahasna08@gmail.com>
2024-09-06 16:05:16 +0700

    complete recipe detail page

--- App.js
+++ App.js
@@ -4,12 +4,25 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import MainScreen from "./src/screens/MainScreen";
 import RecipeDetailScreen from "./src/screens/RecipeDetailScreen";
+import colors from "./src/theme/color";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      theme={{
+        dark: true,
+        colors: {
+          background: colors.background,
+          border: colors.border,
+          notification: colors.notification,
+          primary: colors.primary,
+          text: colors.text,
+          card: colors.card,
+        },
+      }}
+    >
       <Stack.Navigator>
         <Stack.Screen
           name="MainScreen"
--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import RecipeData from "../contants/RecipeData";
 import colors from "../theme/color";
+import { Ionicons } from "@expo/vector-icons";
 
 const RecipeDetailScreen = () => {
   const route = useRoute();
@@ -18,7 +19,78 @@ const RecipeDetailScreen = () => {
         resizeMode="cover"
         style={styles.imageRecipe}
       />
-      
+      <View style={styles.contentContainer}>
+        <Text style={styles.name}>{recipeDetail.name}</Text>
+        <View style={styles.infoContainer}>
+          <View
+            style={[
+              styles.infoContainer,
+              {
+                borderRightWidth: 1,
+              },
+            ]}
+          >
+            <Ionicons name="star" size={24} color={colors.start} />
+            <Text style={styles.info}>{recipeDetail.rating}</Text>
+          </View>
+          <View
+            style={[
+              styles.infoContainer,
+              {
+                paddingLeft: 8,
+                borderRightWidth: 1,
+              },
+            ]}
+          >
+            <Ionicons name="timer-outline" size={24} color={colors.primary} />
+            <Text style={styles.info}>{recipeDetail.cookTimeMinutes} mins</Text>
+          </View>
+          <View
+            style={[
+              styles.infoContainer,
+              {
+                paddingLeft: 8,
+                borderRightWidth: 1,
+              },
+            ]}
+          >
+            <Ionicons
+              name="cellular-outline"
+              size={24}
+              color={colors.primary}
+            />
+            <Text style={styles.info}>{recipeDetail.difficulty}</Text>
+          </View>
+          <View
+            style={[
+              styles.infoContainer,
+              {
+                paddingLeft: 8,
+              },
+            ]}
+          >
+            <Ionicons name="scale-outline" size={24} color={colors.primary} />
+            <Text style={styles.info}>
+              {recipeDetail.caloriesPerServing} Cal
+            </Text>
+          </View>
+        </View>
+        <Text style={styles.sectionTitle}>Ingredients</Text>
+        <View style={styles.ingredientsContainer}>
+          {recipeDetail.ingredients.map((ingredient, index) => (
+            <Text key={index} style={styles.ingredient}>
+              {ingredient}
+              {index !== recipeDetail.ingredients.length - 1 && ", "}
+            </Text>
+          ))}
+        </View>
+        <Text style={styles.sectionTitle}>Instructions</Text>
+        {recipeDetail.instructions.map((instruction, index) => (
+          <Text key={index} style={styles.instruction}>
+            {index + 1}. {instruction}
+          </Text>
+        ))}
+      </View>
     </ScrollView>
   );
 };
@@ -27,9 +99,71 @@ const styles = StyleSheet.create({
   screen: {
     flexGrow: 1,
   },
+  loadingContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
   imageRecipe: {
     backgroundColor: colors.border,
     height: 300,
+  },
+  contentContainer: {
+    flex: 1,
+    marginTop: -16,
+    backgroundColor: colors.background,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingVertical: 20,
+    paddingHorizontal: 24,
+  },
+  name: {
+    fontSize: 24,
+    color: colors.text,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+    paddingRight: 10,
+    borderColor: colors.border,
+    marginBottom: 20,
+  },
+  info: {
+    fontSize: 14,
+    color: colors.primary,
+    marginLeft: 8,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    color: colors.text,
+    marginBottom: 6,
+  },
+  ingredientsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+    flexWrap: "wrap",
+    marginBottom: 20,
+  },
+  ingredient: {
+    fontSize: 16,
+    color: colors.text,
+  },
+  instruction: {
+    fontSize: 16,
+    color: colors.text,
+    marginBottom: 6,
+  },
+  headerBtn: {
+    height: 35,
+    width: 35,
+    borderRadius: 35 / 2,
+    backgroundColor: colors.background,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: colors.border,
+    borderWidth: 1,
   },
 });
 
